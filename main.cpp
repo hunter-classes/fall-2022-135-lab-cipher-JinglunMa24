73068d93260f4afcd5647f49ff565eae513d5fb3
Jinglun Ma <2461447099ma@gmail.com>
2022-11-03 20:13:09 -0400

    added

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "funcs.h"
 
 int main() {
-	std::string s = encryptCaesar("Possibilities of cons", 5);
+	std::string s = encryptCaesar("Life is real.", 5);
 	std::cout << "Encrypted string: " << s << " = " << solve(s) << '\n';
 
 	return 0;
