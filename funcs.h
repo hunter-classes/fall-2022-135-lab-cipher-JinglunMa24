c85fddf2ce1ee8cc214345b18f5ae2d6e7266cf4
Jinglun Ma <2461447099ma@gmail.com>
2022-11-03 20:06:49 -0400

    added functions

--- funcs.h
+++ funcs.h
@@ -1,3 +1,9 @@
 #pragma once
+#include <vector>
 
 // add prototypes here
+char shiftChar(char c, int rshift);
+std::string encryptCaesar(std::string plaintext, int rshift);
+double distance(std::vector<double> alphaFre, std::vector<double> strFre);
+std::vector<double> string_frequency(std::string s);
+std::string solve(std::string encryptStr);
